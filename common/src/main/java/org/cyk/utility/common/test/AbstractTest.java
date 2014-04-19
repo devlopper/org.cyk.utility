fa598b11222e75ae14e1ca5f9633f8092b16a2cb
CYK Developper Home <kycdev@gmail.com>
2014-04-19 08:55:42 +0000

    Change-Id: I90ac56648a0a75155630d672327a5043f0aeb406

--- common/src/main/java/org/cyk/utility/common/test/AbstractTest.java
+++ common/src/main/java/org/cyk/utility/common/test/AbstractTest.java
@@ -2,7 +2,9 @@ package org.cyk.utility.common.test;
 
 import java.io.Serializable;
 
+import org.cyk.utility.common.AbstractMethod;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -12,6 +14,8 @@ public abstract class AbstractTest implements Serializable {
 	private static final long serialVersionUID = -4375668358714913342L;
 	
 	//protected Collection<AbstractTestMethod> methods = new ArrayList<>();
+	
+	protected static AbstractMethod<Object, Object> AFTER_CLASS_METHOD;
 	
 	@Before
 	public final void before() throws Exception {
@@ -24,14 +28,14 @@ public abstract class AbstractTest implements Serializable {
 	protected void _methods_(){}
 	
 	@After
-	public final void after() throws Exception {
-		_after_();
-	}
+	public final void after() throws Exception {_after_();}
+	protected void _after_() throws Exception {}
 	
-	protected void _after_() throws Exception {
-		
+	@AfterClass
+	public static final void afterClass() throws Exception {
+		if(AFTER_CLASS_METHOD!=null)
+			AFTER_CLASS_METHOD.execute();
 	}
-	
 	/**/
 	
 	@Test
--- common/src/main/java/org/cyk/utility/common/test/ArchiveBuilder.java
+++ common/src/main/java/org/cyk/utility/common/test/ArchiveBuilder.java
@@ -1,31 +1,83 @@
 package org.cyk.utility.common.test;
 
+import java.util.HashSet;
+import java.util.Set;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import org.apache.commons.lang3.StringUtils;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+
+@Getter @Setter
 public class ArchiveBuilder {
-/*
-	public enum Layer{MODEL,DAO,SERVICE}
 	
-	private Collection<String> classes = new LinkedHashSet<>();
+	private String packageModelInfix = "model";
+	private String packageApiInfix = "api";
+	private String packageImplInfix = "impl";
 	
-	public ArchiveBuilder model(Class<?> clazz,Layer layer){
-		classes.add(clazz.getName());
-		for(int i=layer.ordinal()-1;i>0;i--){
-			switch(Layer.values()[i]){
-			case MODEL:classes.add(clazz);break;
+	private String serviceImplSuffix = "Impl";
+	
+	private ServiceLayer persistenceServiceLayer = new ServiceLayer("persistence", "Dao");
+	private ServiceLayer businessServiceLayer = new ServiceLayer("business", "Business");
+	
+	private JavaArchive archive;
+	
+	private String jpaPersistenceXml="test-persistence.xml";
+	
+	public ArchiveBuilder create() {
+		archive = ShrinkWrap.create(JavaArchive.class).addAsResource(jpaPersistenceXml,"META-INF/persistence.xml").addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+		return this;
+	}
+	
+	public ArchiveBuilder model(Class<?>[] theModelClasses,ServiceLayer...layers){
+		Set<String> set = new HashSet<>();
+		String model = "."+packageModelInfix+".";
+		for(Class<?> aModelClass : theModelClasses){
+			set.add(aModelClass.getName());
+			if(layers!=null){
+				if(StringUtils.contains(aModelClass.getName(), packageModelInfix)){
+					for(ServiceLayer layer : layers){
+						set.add(StringUtils.replaceOnce(aModelClass.getName(),model,"."+layer.packageInfix+"."+packageApiInfix+".")+layer.metaClassSuffix);
+						set.add(StringUtils.replaceOnce(aModelClass.getName(),model,"."+layer.packageInfix+"."+packageImplInfix+".")+layer.metaClassSuffix+serviceImplSuffix);
+					}		
+				}
 			}
 		}
-		
+		for(String className : set)
+			archive.addClass(className);
+		return this;
+	}
+	
+	public ArchiveBuilder persistence(Class<?>[] aModelClass){
+		return model(aModelClass, persistenceServiceLayer);
+	}
+	
+	public ArchiveBuilder business(Class<?>[] aModelClass){
+		return model(aModelClass, persistenceServiceLayer,businessServiceLayer);
+	}
+	
+	public ArchiveBuilder addClasses(Class<?>... classes) throws IllegalArgumentException {
+		archive.addClasses(classes);
+		return this;
+	}
+
+	public ArchiveBuilder addApi(Class<?>... classes){
+		for(Class<?> clazz : classes){
+			archive.addClass(clazz);
+			archive.addClass(StringUtils.replaceOnce(clazz.getName(),"."+packageApiInfix+".","."+packageImplInfix+".")+serviceImplSuffix);
+		}
 		
 		return this;
 	}
 	
-	public static Archive<?> createDeployment(Class<?>...classes){
-		return ShrinkWrap
-				.create(JavaArchive.class)
-				.addClass(PersistenceService.class).addClass(AbstractPersistenceService.class).addClass(GenericDao.class).addClass(GenericDaoImpl.class)
-				.addClasses(classes)
-				.addAsResource("test-persistence.xml","META-INF/persistence.xml")
-				.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
-	}*/
-	
+
+	@Getter @AllArgsConstructor
+	public static class ServiceLayer{
+		private String packageInfix;
+		private String metaClassSuffix;
+	}
 }
