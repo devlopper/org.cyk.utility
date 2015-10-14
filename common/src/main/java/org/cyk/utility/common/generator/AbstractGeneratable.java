9221cd85774acc9e209c405ec804438d3d524a94
Christian Yao Komenan <christian.komenan@dgbf.gouv.ci>
2015-10-14 14:29:29 +0000

    Change-Id: I85f77d4054d31e4ebdafe3351007d5c1abac87be

--- common/src/main/java/org/cyk/utility/common/generator/AbstractGeneratable.java
+++ common/src/main/java/org/cyk/utility/common/generator/AbstractGeneratable.java
@@ -3,11 +3,16 @@ package org.cyk.utility.common.generator;
 import java.io.ByteArrayInputStream;
 import java.io.Serializable;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public abstract class AbstractGeneratable<T> implements Serializable {
 
 	private static final long serialVersionUID = 6717138845030531852L;
 
 	protected RandomDataProvider provider = RandomDataProvider.getInstance();
+	
+	@Getter @Setter protected Object source;
 	
 	protected ByteArrayInputStream inputStream(byte[] bytes){
 		return new ByteArrayInputStream(bytes);
