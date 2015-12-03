1322961b4ce4e0505a17ad80c20cf70dfcd25a71
devlopper <kycdev@gmail.com>
2015-12-03 15:35:54 +0000

    Change-Id: I6e59b4e90d4ad74f96f33350d83e017f311f7ae8

--- common/src/test/java/org/cyk/utility/common/DatabaseUtilsUT.java
+++ common/src/test/java/org/cyk/utility/common/DatabaseUtilsUT.java
@@ -1,6 +1,8 @@
 package org.cyk.utility.common;
 
 import org.cyk.utility.common.database.DatabaseUtils;
+import org.cyk.utility.common.database.DatabaseUtils.CreateParameters;
+import org.cyk.utility.common.database.DatabaseUtils.DropParameters;
 import org.cyk.utility.common.database.DatabaseUtils.ExportParameters;
 import org.cyk.utility.test.unit.AbstractUnitTest;
 
@@ -13,13 +15,36 @@ public class DatabaseUtilsUT extends AbstractUnitTest {
 	@Override
 	protected void _execute_() {
 		super._execute_();
+		dropDatabase();
+		createDatabase();
 		exportDatabase();
+		dropDatabase();
+	}
+	
+	private void createDatabase(){
+		CreateParameters parameters = new CreateParameters();
+		parameters.setDatabaseName("cyk_testtodel_db");
+		try {
+			databaseUtils.createDatabase(parameters);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void dropDatabase(){
+		DropParameters parameters = new DropParameters();
+		parameters.setDatabaseName("cyk_testtodel_db");
+		try {
+			databaseUtils.dropDatabase(parameters);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	private void exportDatabase(){
 		ExportParameters parameters = new ExportParameters();
-		parameters.setDatabaseName("cyk_iesaschool_db");
-		parameters.setFileName("target/cyk_iesaschool_db_export");
+		parameters.setDatabaseName("cyk_testtodel_db");
+		parameters.setFileName("target/cyk_testtodel_db_export");
 		try {
 			databaseUtils.exportDatabase(parameters);
 		} catch (Exception e) {
