eb96e86449b21ed8aff35009025071f40b47191a
Christian Yao Komenan <christian.komenan@dgbf.gouv.ci>
2015-12-07 10:35:39 +0000

    Change-Id: I78696494ff16ad82a409872a97b901e2379b7976

--- common/src/test/java/org/cyk/utility/common/SimpleUT.java
+++ common/src/test/java/org/cyk/utility/common/SimpleUT.java
@@ -1,9 +1,17 @@
 package org.cyk.utility.common;
 
+import java.util.Date;
+
 import org.cyk.utility.test.unit.AbstractUnitTest;
 
 public class SimpleUT extends AbstractUnitTest {
 
 	private static final long serialVersionUID = -6691092648665798471L;
+	
+	@Override
+	protected void _execute_() {
+		super._execute_();
+		System.out.println("SimpleUT._execute_() : "+new Date());
+	}
 	
 }
