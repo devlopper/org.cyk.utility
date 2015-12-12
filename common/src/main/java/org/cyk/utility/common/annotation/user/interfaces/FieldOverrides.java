8f09670133c3f7968d16182c4200eb452eca9853
devlopper <kycdev@gmail.com>
2015-12-12 21:36:42 +0000

    Change-Id: I56ee26bb5d6a5f6c24366bd5b29e77ce69f0e90c

--- common/src/main/java/org/cyk/utility/common/annotation/user/interfaces/FieldOverrides.java
+++ common/src/main/java/org/cyk/utility/common/annotation/user/interfaces/FieldOverrides.java
@@ -1,13 +1,13 @@
 package org.cyk.utility.common.annotation.user.interfaces;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-//@Target(value={ElementType.TYPE})
-public @interface FieldOverride {
+@Target(value={ElementType.TYPE})
+public @interface FieldOverrides {
 
-	String name();
-	Class<?> type();
-	
+	FieldOverride[] value();
 }
