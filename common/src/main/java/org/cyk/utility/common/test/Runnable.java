2faed5676b0e8185973cd9e7a1d7657f349a03ba
Christian Yao Komenan <christian.komenan@dgbf.gouv.ci>
2016-09-15 16:41:47 +0000

    Change-Id: I4c33dfdff507eb0fe07dabc90f1d9a0cabedb472

---
+++ common/src/main/java/org/cyk/utility/common/test/Runnable.java
@@ -0,0 +1,44 @@
+package org.cyk.utility.common.test;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import org.cyk.utility.common.cdi.AbstractBean;
+import org.cyk.utility.common.cdi.BeanAdapter;
+
+public interface Runnable<LISTENER extends Runnable.Listener> {
+
+	Collection<LISTENER> getListeners();
+					
+    public abstract void run();
+    
+    /**/
+    
+    @Getter @Setter
+    public class Adapter<LISTENER extends Runnable.Listener> extends AbstractBean implements Runnable<LISTENER>,Serializable {
+		private static final long serialVersionUID = 4540958768790791089L;
+    	
+		protected Collection<LISTENER> listeners = new ArrayList<>();
+		
+		@Override
+		public void run() {
+			
+		}
+    }
+    
+    /**/
+    
+    public static interface Listener{
+    	    	
+    	/**/
+    	
+    	public static class Adapter extends BeanAdapter implements Listener,Serializable{
+			private static final long serialVersionUID = -8100384307117008109L;
+
+    	}
+    }
+}
