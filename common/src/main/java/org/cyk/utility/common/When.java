7d7e4cb4fe6f45c690d07c6e07f85aaacd0c641d
Christian Yao Komenan <christian.komenan@dgbf.gouv.ci>
2016-09-16 13:11:38 +0000

    Change-Id: Ia5397ef316d6ec60baa4f03e38d868ba1a16ce90

---
+++ common/src/main/java/org/cyk/utility/common/When.java
@@ -0,0 +1,9 @@
+package org.cyk.utility.common;
+
+public enum When{
+	
+	BEFORE,
+	AFTER
+	
+	;
+}
---
+++ common/src/main/java/org/cyk/utility/common/annotation/user/interfaces/Event.java
@@ -0,0 +1,46 @@
+package org.cyk.utility.common.annotation.user.interfaces;
+
+import java.io.Serializable;
+import java.util.Collection;
+
+import org.cyk.utility.common.ListenerUtils;
+import org.cyk.utility.common.When;
+import org.cyk.utility.common.cdi.BeanAdapter;
+
+public enum Event{
+	CLICK,
+	KEYS
+	
+	;
+	
+	public static interface Listener {
+		
+		void on(Event event,When when);
+		
+		public static class Adapter extends BeanAdapter implements Listener,Serializable {
+			private static final long serialVersionUID = 1L;
+			
+			@Override
+			public void on(Event event, When when) {}
+			
+			/**/
+			
+			public static void listen(Collection<Listener> listeners,final Event event,final When when){
+				ListenerUtils.getInstance().execute(listeners, new ListenerUtils.VoidMethod<Listener>() {
+					@Override
+					public void execute(Event.Listener listener) {
+						listener.on(event, when);
+					}
+				});
+			}
+			public static void listen(Collection<Listener> listeners,Event event,Runnable runnable){
+				listen(listeners,event, When.BEFORE);
+				runnable.run();
+		    	listen(listeners,event, When.AFTER);
+			}
+			
+			
+		}
+		
+	}
+}
