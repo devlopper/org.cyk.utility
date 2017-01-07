7a9d6de7373f1c4a3d67828faf84b72d68a6c7e4
devlopper <kycdev@gmail.com>
2017-01-07 17:13:04 +0000

    Change-Id: Ia38728cfd9e172a01da5da697ee9f7cf544f715d

--- common/src/main/java/org/cyk/utility/common/generator/AbstractGeneratable.java
+++ common/src/main/java/org/cyk/utility/common/generator/AbstractGeneratable.java
@@ -2,6 +2,11 @@ package org.cyk.utility.common.generator;
 
 import java.io.ByteArrayInputStream;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.cyk.utility.common.ListenerUtils;
+import org.cyk.utility.common.cdi.BeanAdapter;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -24,5 +29,51 @@ public abstract class AbstractGeneratable<T> implements Serializable {
 	
 	public abstract void generate();
 	
+	protected String format(final Object object){
+		Object result = ListenerUtils.getInstance().getValue(Object.class, Listener.COLLECTION, new ListenerUtils.ResultMethod<Listener, Object>() {
+
+			@Override
+			public Object execute(Listener listener) {
+				return listener.format(AbstractGeneratable.this, object);
+			}
+
+			@Override
+			public Object getNullValue() {
+				return null;
+			}
+		});
+		if(result==null)
+			return null;
+		return result.toString();
+	}
 	
+	/**/
+	
+	public static interface Listener {
+		Collection<Listener> COLLECTION = new ArrayList<>();
+		
+		Object format(Object object,Object fieldValue);
+		
+		public static class Adapter extends BeanAdapter implements Listener,Serializable {
+			private static final long serialVersionUID = 1L;
+			
+			@Override
+			public Object format(Object object, Object fieldValue) {
+				return null;
+			}
+			
+			public static class Default extends Adapter implements Serializable {
+				private static final long serialVersionUID = 1L;
+				
+				@Override
+				public Object format(Object object, Object fieldValue) {
+					if(fieldValue==null)
+						return null;
+					return fieldValue.toString();
+				}
+				
+			}
+			
+		}
+	}
 }
