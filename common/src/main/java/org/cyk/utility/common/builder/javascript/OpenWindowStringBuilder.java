c0829e0b3e0d038a023dab2604f672e2f4990503
Christian Yao Komenan <christian.komenan@dgbf.gouv.ci>
2017-05-23 16:23:25 +0000

    Change-Id: If6af14cc183ec4cd98ad486f50df40a9f6c34a30

--- common/src/main/java/org/cyk/utility/common/builder/javascript/OpenWindowStringBuilder.java
+++ common/src/main/java/org/cyk/utility/common/builder/javascript/OpenWindowStringBuilder.java
@@ -7,6 +7,7 @@ import java.util.Collection;
 import org.cyk.utility.common.Constant;
 import org.cyk.utility.common.builder.NameValueCollectionStringBuilder;
 import org.cyk.utility.common.builder.TextStringBuilder;
+import org.cyk.utility.common.builder.UrlStringBuilder;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,30 +20,47 @@ public class OpenWindowStringBuilder extends AbstractJavascriptStringBuilder imp
 	
 	private static final String FORMAT = "window.open('%s','%s','%s','%s')";
 	
-	private String url, name;
+	private UrlStringBuilder urlStringBuilder;
+	private String name;
 	private Integer leftIndex,topIndex,width,height;
 	private Boolean replaced,showMenuBar=Boolean.TRUE,showStatusBar=Boolean.TRUE,showTitleBar=Boolean.TRUE,showToolBar=Boolean.TRUE;
 	
 	public OpenWindowStringBuilder(String name,String url){
 		this.name = name;
-		this.url = url;
+		getUrlStringBuilder().setInstance(url);
+	}
+	
+	public OpenWindowStringBuilder(String name){
+		this(name,null);
+	}
+	
+	public UrlStringBuilder getUrlStringBuilder(){
+		if(urlStringBuilder==null)
+			urlStringBuilder = new UrlStringBuilder();
+		return urlStringBuilder;
 	}
 	
 	@Override
 	public String buildWhenBlank() {
 		NameValueCollectionStringBuilder nameValueCollectionStringBuilder = new NameValueCollectionStringBuilder();
+		if(showToolBar==null)
+			showToolBar = Boolean.TRUE;
+		if(leftIndex==null)
+			leftIndex = 200;
+		if(topIndex==null)
+			topIndex=50;
+		if(width==null)
+			width = 900;
+		if(height==null)
+			height = 500;
 		nameValueCollectionStringBuilder.setSeparator(Constant.CHARACTER_COMA.toString()).addNamesValues("toolbar",inject(TextStringBuilder.class).setResponse(showToolBar).build()
 				,"top",topIndex,"left",leftIndex,"width",width,"height",height);
-		return String.format(FORMAT, url,name,nameValueCollectionStringBuilder.build(),inject(TextStringBuilder.class).setResponse(replaced).build());
+		
+		//TODO should depends on windows _top _blank and so on
+		getUrlStringBuilder().getQueryStringBuilder().getNameValueCollectionStringBuilder().addDialog();
+		
+		return String.format(FORMAT, urlStringBuilder.build(),name,nameValueCollectionStringBuilder.build(),inject(TextStringBuilder.class).setResponse(replaced).build());
 	}
-	
-	/*private <T> Object getValue(Class<T> aClass,T value){
-		if(Boolean.class.equals(aClass))
-			return Boolean.TRUE.equals(value) ? "yes" : "no";
-		if(Number.class.isAssignableFrom(aClass))
-			return value;
-		return Constant.EMPTY_STRING;
-	}*/
 	
 	/**/
 	
