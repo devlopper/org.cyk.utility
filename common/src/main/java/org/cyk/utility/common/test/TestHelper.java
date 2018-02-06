dd3adf26024b0bd1c1f27dfc05e886f579772a53
Christian Yao Komenan <Christian@CYK-HP-LAPTOP>
2018-02-06 06:05:30 +0000

    Change-Id: I28709177303ba79502c29bd76f6b53c312345d0b

---
+++ common/src/main/java/org/cyk/utility/common/test/TestHelper.java
@@ -0,0 +1,35 @@
+package org.cyk.utility.common.test;
+
+import java.io.Serializable;
+
+import javax.inject.Singleton;
+
+import org.cyk.utility.common.cdi.AbstractBean;
+import org.cyk.utility.common.helper.AbstractHelper;
+
+@Singleton
+public class TestHelper extends AbstractHelper implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private static TestHelper INSTANCE;
+	
+	public static TestHelper getInstance() {
+		if(INSTANCE == null)
+			INSTANCE = new TestHelper();
+		return INSTANCE;
+	}
+	
+	@Override
+	protected void initialisation() {
+		INSTANCE = this;
+		super.initialisation();
+	}
+	
+	/**/
+	
+	public static class TestCase extends AbstractBean implements Serializable {
+		private static final long serialVersionUID = 1L;
+		
+	}
+	
+}
