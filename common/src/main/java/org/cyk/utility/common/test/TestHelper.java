e9f65951f489540bccb35a152ce1ba04dfdc74c3
Christian Yao Komenan <Christian@CYK-HP-LAPTOP>
2018-02-23 08:23:42 +0000

    Change-Id: I265b6c4fc1b80923937afc9fd53fc89601b8227c

--- common/src/main/java/org/cyk/utility/common/test/TestHelper.java
+++ common/src/main/java/org/cyk/utility/common/test/TestHelper.java
@@ -6,6 +6,8 @@ import javax.inject.Singleton;
 
 import org.cyk.utility.common.cdi.AbstractBean;
 import org.cyk.utility.common.helper.AbstractHelper;
+import org.cyk.utility.common.helper.ClassHelper;
+import org.cyk.utility.common.helper.RandomHelper;
 
 @Singleton
 public class TestHelper extends AbstractHelper implements Serializable {
@@ -29,6 +31,16 @@ public class TestHelper extends AbstractHelper implements Serializable {
 	
 	public static class TestCase extends AbstractBean implements Serializable {
 		private static final long serialVersionUID = 1L;
+		
+		public <T> T instanciateOne(Class<T> aClass){
+			return ClassHelper.getInstance().instanciateOne(aClass);
+		}
+		
+		/* shortcuts */
+		
+		public RandomHelper getRandomHelper(){
+			return RandomHelper.getInstance();
+		}
 		
 	}
 	
