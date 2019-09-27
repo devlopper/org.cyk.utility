c98d7a35e01c01054e171443574f30bdee248345
Christian Yao Komenan <kycdev@gmail.com>
2019-09-27 17:13:23 +0000

    commit

--- jee-utility-server-representation/src/test/java/org/cyk/utility/server/representation/entities/NodeDtoMapper.java
+++ jee-utility-server-representation/src/test/java/org/cyk/utility/server/representation/entities/NodeDtoMapper.java
@@ -5,7 +5,7 @@ import org.cyk.utility.server.persistence.entities.Nodes;
 import org.cyk.utility.server.representation.hierarchy.AbstractNodeMapperImpl;
 import org.mapstruct.Mapper;
 
-@Mapper
+//@Mapper
 public abstract class NodeDtoMapper extends AbstractNodeMapperImpl<NodeDto, Node,NodeDtoCollection,Nodes> {
 	private static final long serialVersionUID = 1L;
 	
--- jee-utility-server-representation/src/test/java/org/cyk/utility/server/representation/entities/NodeHierarchyDtoMapper.java
+++ jee-utility-server-representation/src/test/java/org/cyk/utility/server/representation/entities/NodeHierarchyDtoMapper.java
@@ -5,7 +5,7 @@ import org.cyk.utility.server.persistence.entities.NodeHierarchy;
 import org.cyk.utility.server.representation.hierarchy.AbstractHierarchyMapperImpl;
 import org.mapstruct.Mapper;
 
-@Mapper
+//@Mapper
 public abstract class NodeHierarchyDtoMapper extends AbstractHierarchyMapperImpl<NodeHierarchyDto, NodeHierarchy,NodeDto,Node,NodeDtoMapper> {
 	private static final long serialVersionUID = 1L;
     
