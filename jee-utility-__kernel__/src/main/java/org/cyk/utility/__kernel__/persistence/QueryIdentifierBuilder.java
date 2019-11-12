deb66be8137346f37b767e7184c9408b02b7fa1f
Christian Yao Komenan <kycdev@gmail.com>
2019-11-12 23:13:07 +0000

    commit

---
+++ jee-utility-__kernel__/src/main/java/org/cyk/utility/__kernel__/persistence/QueryIdentifierBuilder.java
@@ -0,0 +1,67 @@
+package org.cyk.utility.__kernel__.persistence;
+
+import java.io.Serializable;
+
+import org.apache.commons.lang3.StringUtils;
+import org.cyk.utility.__kernel__.DependencyInjection;
+import org.cyk.utility.__kernel__.log.LogHelper;
+import org.cyk.utility.__kernel__.string.StringHelper;
+import org.cyk.utility.__kernel__.value.Value;
+import org.cyk.utility.__kernel__.value.ValueHelper;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+public interface QueryIdentifierBuilder {
+
+	default String build(Parameters parameters) {
+		if(parameters == null)
+			return null;
+		String classSimpleName = parameters.getClassSimpleName();
+		if(StringHelper.isBlank(classSimpleName)){
+			if(Boolean.TRUE.equals(parameters.getIsDerivedFromQueryIdentifier())){
+				classSimpleName =  StringUtils.substringBefore(parameters.getDerivedFromQueryIdentifier(), ".");
+			}
+		}
+		String name = parameters.getName();
+		if(StringHelper.isBlank(name)){
+			if(Boolean.TRUE.equals(parameters.getIsDerivedFromQueryIdentifier())){
+				name =  StringUtils.substringAfter(parameters.getDerivedFromQueryIdentifier(), ".");
+				if(Boolean.TRUE.equals(parameters.getIsCountInstances()))
+					//replace first occurence of read by count
+					name = StringUtils.replaceOnce(name, "read", "count");
+			}
+		}
+		String format = ValueHelper.defaultToIfBlank(parameters.getFormat(),FORMAT);
+		return String.format(format, classSimpleName,name);
+	}
+	
+	default String build(Class<?> klass,String name) {
+		if(klass == null || StringHelper.isBlank(name))
+			return null;
+		return build(new Parameters().setClassSimpleName(klass.getSimpleName()).setName(name));
+	}
+	
+	/**/
+	
+	static QueryIdentifierBuilder getInstance() {
+		QueryIdentifierBuilder instance = (QueryIdentifierBuilder) INSTANCE.get();
+		if(instance != null)
+			return instance;
+		INSTANCE.set(instance = DependencyInjection.inject(QueryIdentifierBuilder.class));
+		LogHelper.logInfo("instance has been set. <<"+instance.getClass()+">>", QueryIdentifierBuilder.class);
+		return instance;
+	}
+	
+	Value INSTANCE = DependencyInjection.inject(Value.class);
+	String FORMAT = "%s.%s";
+	
+	@Getter @Setter @Accessors(chain=true)
+	public static class Parameters implements Serializable {
+		private static final long serialVersionUID = 1L;
+		
+		private Boolean isDerivedFromQueryIdentifier,isCountInstances;
+		private String name,derivedFromQueryIdentifier,classSimpleName,format = FORMAT;
+	}
+}
---
+++ jee-utility-__kernel__/src/main/java/org/cyk/utility/__kernel__/persistence/QueryIdentifierBuilderImpl.java
@@ -0,0 +1,8 @@
+package org.cyk.utility.__kernel__.persistence;
+
+import java.io.Serializable;
+
+public class QueryIdentifierBuilderImpl extends AbstractQueryIdentifierBuilderImpl implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+}
