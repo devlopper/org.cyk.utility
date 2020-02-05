903f9aa6ef8f6025889a5fe1c0e940c5d49a531a
Christian Yao Komenan <kycdev@gmail.com>
2020-02-05 15:51:53 +0000

    commit

--- jee-utility-__kernel__/src/main/java/org/cyk/utility/__kernel__/instance/InstanceCopierImpl.java
+++ jee-utility-__kernel__/src/main/java/org/cyk/utility/__kernel__/instance/InstanceCopierImpl.java
@@ -2,6 +2,9 @@ package org.cyk.utility.__kernel__.instance;
 
 import java.io.Serializable;
 
+import javax.enterprise.context.Dependent;
+
+@Dependent
 public class InstanceCopierImpl extends AbstractInstanceCopierImpl implements Serializable {
 	private static final long serialVersionUID = 1L;
 
