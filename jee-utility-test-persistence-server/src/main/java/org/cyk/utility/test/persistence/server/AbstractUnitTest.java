cc816e04d2d8cbf78460875266a444c88d99c10c
Christian Yao Komenan <kycdev@gmail.com>
2021-03-02 15:12:18 +0000

    commit

---
+++ jee-utility-test-persistence-server/src/main/java/org/cyk/utility/test/persistence/server/AbstractUnitTest.java
@@ -0,0 +1,24 @@
+package org.cyk.utility.test.persistence.server;
+
+import javax.persistence.Persistence;
+
+import org.cyk.utility.persistence.EntityManagerFactoryGetterImpl;
+import org.cyk.utility.test.weld.AbstractWeldUnitTest;
+
+public abstract class AbstractUnitTest extends AbstractWeldUnitTest {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void __listenBefore__() {
+		super.__listenBefore__();
+		initializeEntityManagerFactory(getPersistenceUnitName());
+	}
+	
+	protected void initializeEntityManagerFactory(String persistenceUnitName) {
+		EntityManagerFactoryGetterImpl.ENTITY_MANAGER_FACTORY = Persistence.createEntityManagerFactory(persistenceUnitName);
+	}
+	
+	protected String getPersistenceUnitName() {
+		return "default";
+	}
+}
