ae9a420283473beb98e915acf5b281f90b4462d8
Christian Yao Komenan <kycdev@gmail.com>
2021-05-21 23:10:52 +0000

    commit

--- jee-utility-persistence-server/src/main/java/org/cyk/utility/persistence/server/query/ArraysReaderByIdentifiers.java
+++ jee-utility-persistence-server/src/main/java/org/cyk/utility/persistence/server/query/ArraysReaderByIdentifiers.java
@@ -9,6 +9,7 @@ import javax.persistence.Query;
 import org.cyk.utility.__kernel__.array.ArrayHelper;
 import org.cyk.utility.__kernel__.collection.CollectionHelper;
 import org.cyk.utility.__kernel__.field.FieldHelper;
+import org.cyk.utility.__kernel__.log.LogHelper;
 import org.cyk.utility.persistence.EntityManagerGetter;
 
 public interface ArraysReaderByIdentifiers<ENTITY,IDENTIFIER> extends Reader<ENTITY, IDENTIFIER, Object[]> {
@@ -17,6 +18,9 @@ public interface ArraysReaderByIdentifiers<ENTITY,IDENTIFIER> extends Reader<ENT
 	Collection<Object[]> read(Map<String,Object> parameters,ENTITY...entities);
 	Collection<Object[]> readByIdentifiers(Collection<IDENTIFIER> identifiers,Map<String,Object> parameters);
 	Collection<Object[]> readByIdentifiers(Map<String,Object> parameters,IDENTIFIER...identifiers);
+	
+	void set(ENTITY entity,Object[] array);
+	void set(Collection<ENTITY> entities,Collection<Object[]> arrays);
 	
 	/**/
 	
@@ -64,6 +68,31 @@ public interface ArraysReaderByIdentifiers<ENTITY,IDENTIFIER> extends Reader<ENT
 			if(CollectionHelper.isEmpty(entities))
 				return null;
 			return readByIdentifiers((Collection<IDENTIFIER>) FieldHelper.readSystemIdentifiers(entities),parameters);
+		}
+		
+		@Override
+		public void set(ENTITY entity,Object[] array) {
+			if(entity == null || array == null || array.length == 0)
+				return;
+			__set__(entity, array);
+		}
+		
+		protected void __set__(ENTITY entity,Object[] array) {
+			LogHelper.logWarning("instantiate not yet implemented", getClass());
+		}
+		
+		@Override
+		public void set(Collection<ENTITY> entities,Collection<Object[]> arrays) {
+			if(CollectionHelper.isEmpty(entities) || CollectionHelper.isEmpty(arrays))
+				return;
+			for(ENTITY entity : entities) {
+				for(Object[] array : arrays) {
+					if(array[0].equals(FieldHelper.readSystemIdentifier(entity))) {
+						__set__(entity, array);
+						break;
+					}
+				}
+			}
 		}
 		
 		/**/
