2d5ca5d6c2d2c315e21ded72e0d501065b8a99c3
devlopper <kycdev@gmail.com>
2022-09-01 13:55:35 +0000

    commit

--- jee-utility-mail-postmark/src/test/java/org/cyk/utility/mail/postmark/MainTest.java
+++ jee-utility-mail-postmark/src/test/java/org/cyk/utility/mail/postmark/MainTest.java
@@ -11,11 +11,18 @@ import com.wildbit.java.postmark.client.exception.PostmarkException;
 public class MainTest {
 
 	public static void main(String[] args) throws PostmarkException, IOException {	
-		ApiClient client = Postmark.getApiClient("fa2f17e3-b4ef-492e-87c5-37793f9105ca");
-		Message message = new Message("votre-bulletin-de-solde@sigobe.dgbf.ci", "c.komenan@budget.gouv.ci", "Hello from Postmark!", "Hello message body");
+		/*
+		ApiClient client = Postmark.getApiClient("ca8c7c8e-b623-43c0-bf0a-ba319878f63a");
+		Message message = new Message("assistance.sigobe@budget.gouv.ci", "c.komenan@budget.gouv.ci", "Hello from Postmark!", "Hello message body");
 		MessageResponse response = client.deliverMessage(message);
 		System.out.println(response.getErrorCode());
 		System.out.println("DONE");
+		*/
+		
+		ApiClient client = Postmark.getApiClient("ca8c7c8e-b623-43c0-bf0a-ba319878f63a");
+		Message message = new Message("c.komenan@budget.gouv.ci", "assistance.sigobe@budget.gouv.ci", "Hello from Postmark!", "Hello message body");
+		message.setMessageStream("outbound");
+		MessageResponse response = client.deliverMessage(message);
 	}
 
 }
